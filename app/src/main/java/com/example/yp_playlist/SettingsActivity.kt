3d988d9a4021ab0953367babc691efe14097c2c8
Aleksey Golyudov <a.golyudov@agro.club>
2023-03-07 10:50:29 +0300

    Playlist_maker sprint 9

--- app/src/main/java/com/example/yp_playlist/SettingsActivity.kt
+++ app/src/main/java/com/example/yp_playlist/SettingsActivity.kt
@@ -2,8 +2,10 @@ package com.example.yp_playlist
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 
@@ -16,6 +18,34 @@ class SettingsActivity : AppCompatActivity() {
         homeButton.setOnClickListener {
             finish()
         }
+        val shareButton = findViewById<LinearLayout>(R.id.Share)
+        shareButton.setOnClickListener {
+            val androidDevelopment = getString(R.string.android_development_course)
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.type = Uri.parse("text/plain").toString()
+            shareIntent.putExtra(Intent.EXTRA_TEXT, androidDevelopment)
+            val chooserIntent = Intent.createChooser(shareIntent, null)
+            startActivity(chooserIntent)
+        }
+
+        val supportButton = findViewById<LinearLayout>(R.id.Support)
+        supportButton.setOnClickListener{
+            val callSupport = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:")
+                putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.supportMail)))
+                putExtra(Intent.EXTRA_SUBJECT, getString(R.string.mailSubject))
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.supportMessage))
+            }
+            startActivity(callSupport)
+        }
+
+        val agreementButton = findViewById<LinearLayout>(R.id.Agreement)
+        agreementButton.setOnClickListener {
+            val openPage = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.agreement)))
+            startActivity(openPage)
+        }
+
+
     }
 }
 
